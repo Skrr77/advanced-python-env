3738232609ac73544583fdad227e4ce92be268da
Skrr77 <Alinur880@mail.ru>
2025-12-30 21:56:35 +0500

    Add files via upload

---
+++ assignment 3/task1.py
@@ -0,0 +1,23 @@
+import math
+
+# 1. Area of geometric shapes
+def circle_area(r):
+    return math.pi * r * r
+
+def rectangle_area(a, b):
+    return a * b
+
+def triangle_area(a, h):
+    return 0.5 * a * h
+
+# 2. Sum and arithmetic mean of arrays
+arrays = [
+    [1, 2, 3],
+    [4, 5, 6, 7],
+    [10, 20]
+]
+
+for arr in arrays:
+    s = sum(arr)
+    mean = s / len(arr)
+    print(f"Array: {arr}, Sum = {s}, Mean = {mean}")
